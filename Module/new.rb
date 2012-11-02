d2f572eb6eceecac187636ee5a3af55ecef26f1c
Tomohiko Himura <eiel.hal@gmail.com>
2012-11-02 14:58:10 +0900

    追記

--- Module/new.rb
+++ Module/new.rb
@@ -4,5 +4,29 @@ m.name                          # => nil
 Hoge = m
 m.name                          # => "Hoge"
 Mogu = m
-# 名前は変わらないっぽい
+# 名前は変わらない
 m.name                          # => "Hoge"
+
+Hoge = Module.new # !> already initialized constant Hoge
+
+module Hoge; end
+module Hoge
+  module Mogu
+    Anything = "hoge"
+    def hoge
+      1
+    end
+    module_function :hoge
+  end
+end
+
+H = Module.new do
+  def goro
+    2
+  end
+end
+
+include H
+
+goro                            # => 2
+Hoge::Mogu.hoge                 # => 1
